bb6cbacd744536a274d824fb03ebdfbb557113ba
Jeryhardianto <hardiantojr292gmail.com>
2022-11-14 09:09:46 +0800

    Fix laporan pinjam dan pengembalian

--- resources/views/pages/backsite/laporan/lapasset.blade.php
+++ resources/views/pages/backsite/laporan/lapasset.blade.php
@@ -94,17 +94,9 @@
                 "responsive": true,
                 "lengthChange": false,
                 "autoWidth": false,
-                "buttons": ["excel", "pdf", "print"]
+                "buttons": ["excel", "print"]
             }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
-            $('#example2').DataTable({
-                "paging": true,
-                "lengthChange": false,
-                "searching": false,
-                "ordering": true,
-                "info": true,
-                "autoWidth": false,
-                "responsive": true,
-            });
+           
         });
     </script>
 @endpush
--- resources/views/pages/backsite/laporan/lapstok.blade.php
+++ resources/views/pages/backsite/laporan/lapstok.blade.php
@@ -79,7 +79,7 @@
                 "responsive": true,
                 "lengthChange": false,
                 "autoWidth": false,
-                "buttons": ["excel", "pdf", "print"]
+                "buttons": ["excel","print"]
             }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
         });
     </script>
